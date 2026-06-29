9a97798ae06be0334dcaf3b2f36f67b8612e3df7
adept132 <botsman.d132@gmail.com>
2026-06-29 21:12:22 +0300

    Fixed firebase auth

--- api/core/firebase_admin.py
+++ api/core/firebase_admin.py
@@ -1,15 +1,24 @@
 import os
+import json
 
 import firebase_admin
 from firebase_admin import auth, credentials
 
-FIREBASE_SERVICE_ACCOUNT_PATH = os.getenv(
-    "FIREBASE_SERVICE_ACCOUNT_PATH",
-    "api/firebase/fitpilot-6b389-firebase-adminsdk-fbsvc-12056c2e01.json",
-)
+# Локальный путь оставляем для разработки на твоем компе
+LOCAL_SERVICE_ACCOUNT_PATH = "api/firebase/fitpilot-6b389-firebase-adminsdk-fbsvc-12056c2e01.json"
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT_PATH)
+    # 1. Пытаемся достать JSON прямо из переменной окружения (сработает на Render)
+    firebase_creds_str = os.getenv("FIREBASE_CREDENTIALS")
+
+    if firebase_creds_str:
+        # Парсим строку в словарь и отдаем Firebase
+        creds_dict = json.loads(firebase_creds_str)
+        cred = credentials.Certificate(creds_dict)
+    else:
+        # 2. Если переменной нет, читаем из файла (сработает у тебя локально)
+        cred = credentials.Certificate(LOCAL_SERVICE_ACCOUNT_PATH)
+
     firebase_admin.initialize_app(cred)
 
 
